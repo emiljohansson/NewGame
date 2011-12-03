705228e17d0c54b030b05821eef7ed27b63eae59
Kamil Trebunia <kamil.trebunia@gmail.com>
2011-12-03 12:47:51 +0100

    Added .DS_Store to .gitignore

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log("New Game");
+var newgame = {};
+
+newgame.MAP_URL = "map.json";
+
+newgame.SPRITE_URL = "sprite.png";
+newgame.TILE_WIDTH = 64;
+newgame.TILE_HEIGHT = 64;
+
+newgame.initCanvas = function () {
+	
+	var canvas = document.createElement("canvas");
+
+};
+
+newgame.net = {};
+newgame.net.get = function (url, callback) {
+
+	var httpRequest = new XMLHttpRequest();
+
+	httpRequest.onreadystatechange = function () {
+		if (httpRequest.readyState === 4) {
+			if (httpRequest.status === 200) {
+				callback(httpRequest.responseText);
+			}
+		}
+	};
+
+	httpRequest.open("GET", url);
+	httpRequest.send();
+
+};
+
+window.addEventListener("load", function () {
+
+	var sprite = new Image();
+
+	sprite.addEventListener("load", newgame.init, false);
+
+	sprite.setAttribute("src", newgame.SPRITE_URL);
+
+}, false);
