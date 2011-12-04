c8f96affac2db53bed6d4e5288a7adcd0811fd14
Kamil Trebunia <kamil.trebunia@gmail.com>
2011-12-04 13:53:50 +0100

    Upgraded assets loader for new tile set

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ var newgame = {};
 
 newgame.MAP_URL = "map.json";
 
-newgame.SPRITE_URL = "sprite.png";
 newgame.SPRITE_MAP_URL = "sprite.json";
-newgame.TILE_WIDTH = 64;
-newgame.TILE_HEIGHT = 64;
+newgame.TILE_WIDTH = 101;
+newgame.TILE_HEIGHT = 171;
+newgame.TILE_THICKNESS = 40;
+newgame.TILE_OFFSET = 50;
 
 window.addEventListener("load", function () {
 
@@ -13,34 +14,51 @@ window.addEventListener("load", function () {
 
 	var mapDeferred = newgame.net.getJSON(newgame.MAP_URL);
 
-	// Get sprite
-
-	var spriteDeferred = new newgame.utils.Deferred();
-
-	var sprite = new Image();
-
-	sprite.addEventListener("load", function () {
-		spriteDeferred.callback();
-	}, false);
-
-	sprite.setAttribute("src", newgame.SPRITE_URL);
-
 	// Get sprite map
 
 	var spriteMapDeferred = newgame.net.getJSON(newgame.SPRITE_MAP_URL);
 
+	// Load sprites
+
+	var imagesDeferred = new newgame.utils.Deferred();
+
+	spriteMapDeferred.addCallback(function (spriteMap) {
+
+		var objectsURIs = Object.keys(spriteMap);
+		var loadedObjectsNumber = 0;
+
+		objectsURIs.forEach(function (uri) {
+
+			spriteMap[uri].image = new Image();
+			spriteMap[uri].image.addEventListener("load", function () {
+				if (++loadedObjectsNumber === objectsURIs.length) {
+					imagesDeferred.callback();
+				}
+			}, false);
+			spriteMap[uri].image.addEventListener("error", function () {
+				imagesDeferred.errback();
+			}, false);
+			spriteMap[uri].image.setAttribute("src", spriteMap[uri].url);
+
+		});
+
+	});
+
 	// When all is ready, init the game
 
-	newgame.utils.Deferred.gatherResults([mapDeferred, spriteDeferred, spriteMapDeferred]).addCallback(function () {
+	newgame.utils.Deferred.gatherResults([
+		mapDeferred,
+		imagesDeferred,
+		spriteMapDeferred
+	]).addCallback(function () {
 		
 		var core = new newgame.Core({
-				map: mapDeferred.result[0]
+				map: mapDeferred.result[0],
+				entityTypes: spriteMapDeferred.result[0]
 			});
 
 		var view = new newgame.View({
-				core: core,
-				sprite: sprite,
-				spriteMap: spriteMapDeferred.result[0]
+				core: core
 			});
 
 	});
