469dc2423cd13ce643924a4c1cbd81437766e175
Kamil Trebunia <kamil.trebunia@gmail.com>
2011-12-04 14:07:23 +0100

    "HelloWorld" map rendering

--- Core.js
+++ Core.js
@@ -1,7 +1,12 @@
 (function () {
 	
 	newgame.Core = function (config) {
+
+		this.map = config.map;
+		this.entityTypes = config.entityTypes;
+
 		console.log("core initialized", config);
+
 	};
 
 })();
--- View.js
+++ View.js
@@ -1,7 +1,31 @@
 (function () {
 	
 	newgame.View = function (config) {
+
+		this.core = config.core;
+
+		var canvas = document.createElement("canvas");
+
+		canvas.width = this.core.map.width * newgame.TILE_WIDTH;
+		canvas.height = this.core.map.height * (newgame.TILE_HEIGHT - newgame.TILE_THICKNESS - newgame.TILE_OFFSET) + newgame.TILE_THICKNESS;
+
+		document.body.appendChild(canvas);
+
+		var ctx = canvas.getContext("2d");
+
+		this.core.map.entities.forEach(function (entityConfig) {
+
+			var entity = this.core.entityTypes[entityConfig.uri];
+
+			ctx.drawImage(
+					entity.image,
+					entityConfig.x * newgame.TILE_WIDTH,
+					entityConfig.y * (newgame.TILE_HEIGHT - newgame.TILE_THICKNESS - newgame.TILE_OFFSET) - entityConfig.z * newgame.TILE_THICKNESS - newgame.TILE_OFFSET);
+
+		}, this);
+
 		console.log("view initialized", config);
+
 	};
 
 })();
